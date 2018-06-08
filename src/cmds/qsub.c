8ddae20432122c50f02c493d78060e53884b756f
William Meng <wlm2117@columbia.edu>
2018-06-08 15:13:16 -0700

    Fix format of function declaration and opening brace

--- src/cmds/qsub.c
+++ src/cmds/qsub.c
@@ -4675,7 +4675,6 @@ handle_attribute_errors(struct ecl_attribute_errors *err_list,
 	return 0;
 }
 
-/* Try to submit via daemon */
 #ifdef WIN32
 /*
  * @brief
@@ -4688,7 +4687,9 @@ handle_attribute_errors(struct ecl_attribute_errors *err_list,
  * @param[out] do_regular_submit - Indicate whether to do regular submit
  * @return     rc                - Error code
  */
-int daemon_submit(const char *qsub_exe, int *do_regular_submit) {
+int
+daemon_submit(const char *qsub_exe, int *do_regular_submit)
+{
 	int rc = 0;
 	/* determine pipe name */
 	get_comm_filename(fl);
@@ -4813,7 +4814,9 @@ again:
  * @param[out] do_regular_submit - Indicate whether to do regular submit
  * @return     rc                - Error code
  */
-int daemon_submit(int *daemon_up, int *do_regular_submit) {
+int
+daemon_submit(int *daemon_up, int *do_regular_submit)
+{
 	int    sock; /* UNIX domain socket for talking to daemon */
 	struct sockaddr_un   s_un;
 	sigset_t newsigmask;
@@ -4911,7 +4914,9 @@ again:
  * @param[in] daemon_up - Indicates whether daemon is running
  * @return    rc        - Error code
  */
-int regular_submit(const int daemon_up) {
+int
+regular_submit(const int daemon_up)
+{
 	int rc = 0;
 	rc = do_connect(server_out, retmsg);
 	if (rc == 0) {
