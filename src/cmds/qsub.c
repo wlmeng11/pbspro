3cab64e38c06afda00263946e8aba5008943f819
William Meng <wlm2117@columbia.edu>
2018-06-07 17:51:44 -0700

    Add comment and rearrange whitespace

--- src/cmds/qsub.c
+++ src/cmds/qsub.c
@@ -4761,6 +4761,7 @@ main(int argc, char **argv, char **envp)   /* qsub */
 		exit(0);
 	}
 #endif
+
 	strcpy(qsub_exe, argv[0]); /* note the name of the qsub executable */
 
 	/*
@@ -4774,8 +4775,8 @@ main(int argc, char **argv, char **envp)   /* qsub */
 		cmdargs = NULL;
 	}
 
+	/* Process arguments */
 	errflg = process_opts(argc, argv, CMDLINE);  /* get cmd-line options */
-
 	if (errflg) {
 		print_usage();
 		exit_qsub  (2);
@@ -4923,11 +4924,9 @@ main(int argc, char **argv, char **envp)   /* qsub */
 	/* Get required environment variables to be sent to the server. */
 	/* Must be done early here, as basic_envlist and qsub_envlist will */
 	/* be sent to the qsub daemon if needed */
-
 	basic_envlist = job_env_basic();
 	if (basic_envlist == NULL)
 		exit_qsub(3);
-
 	if (V_opt)
 		qsub_envlist = env_array_to_varlist(envp);
 
@@ -4944,6 +4943,7 @@ main(int argc, char **argv, char **envp)   /* qsub */
 	 */
 	if (Interact_opt || block_opt || no_background)
 		goto regular_submit;
+
 
 #ifdef WIN32
 	/* determine pipe name */
